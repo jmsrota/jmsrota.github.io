d24ffd2c45450a6e271cadda8ebc46c830a286e4
jmsrota <92889640+jmsrota@users.noreply.github.com>
2025-11-25 18:12:34 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue' // or react() if using React
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: './', // important for GitHub Pages
-  plugins: [vue()],
+  plugins: [vue(), react()],
   build: {
     outDir: 'dist'
   }
